66dcb10ddf634ca6e95e7860489a360d731cad74
neagix <neagix@users.noreply.github.com>
2017-08-20 19:10:53 +0200

    Implemented stubs for all the supported options

--- config.c
+++ config.c
@@ -83,6 +83,10 @@ void config_load(void) {
 	gfx_printf("configuration loaded successfully\n");
 }
 
+int isspace(char c) {
+	return ((c == ' ') || (c == '\t'));
+}
+
 void trim_right(char *s, char *beginning) {
 	s--;
 	while (1) {
@@ -90,7 +94,7 @@ void trim_right(char *s, char *beginning) {
 			return;
 		if (*s == 0x0)
 			return;
-		if ((*s == ' ') ||(*s == '\t')) {
+		if (isspace(*s)) {
 			*s = 0x0;
 			s--;
 		} else
@@ -98,85 +102,9 @@ void trim_right(char *s, char *beginning) {
 	}
 }
 
-int parse_timeout(char *s);
-int parse_default(char *s);
-int parse_title(char *s);
-
 #define ERR_MISSING_TOKEN 	0x10
 #define ERR_INVALID_NUMBER	0x20
-
-int process_line(char *line) {
-	//gfx_printf("first line: %s\n", last_line);
-	
-	// skip whitespace
-	while ((*line == ' ') || (*line == '\t'))
-		line++;
-	if (!*line)
-		return 0;
-	
-	// is this a comment?
-	if (*line == '#')
-		return 0;
-		
-	// find next space or end of line
-	char *eot = line;
-	int eol_reached = 0;
-	while (1) {
-		if (!*eot) {
-			eol_reached = 1;
-			break;
-		}
-		if ((*eot != ' ') && (*eot != '\t'))
-			eot++;
-		else {
-			*eot = 0x0;
-			eot++;
-			break;
-		}
-	}
-	
-	// skip some more leading whitespaces
-	if (!eol_reached) {
-		while (1) {
-			if (!*eot) {
-				break;
-			}
-			if ((*eot == ' ') || (*eot == '\t'))
-				eot++;
-			else
-				break;
-		}
-	}
-	
-	trim_right(eot, line);
-	
-	if (0 == strcmp(line, "timeout")) {
-		if (eol_reached) {
-			return ERR_MISSING_TOKEN;
-		}
-		
-		return parse_timeout(eot);
-	} else if (0 == strcmp(line, "default")) {
-		if (eol_reached) {
-			return ERR_MISSING_TOKEN;
-		}
-		
-		return parse_default(eot);
-	} else if (0 == strcmp(line, "title")) {
-		if (eol_reached) {
-			return ERR_MISSING_TOKEN;
-		}
-		
-		return parse_title(eot);
-	} else {
-		gfx_printf("unknown token: %s\n", line);
-	}
-	
-//	if (eol_reached)
-//		return 0;
-	
-	return 0;
-}
+#define ERR_EXTRA_TOKEN 	0x30
 
 int parse_timeout(char *s) {
 	size_t parsed;
@@ -202,6 +130,160 @@ int parse_default(char *s) {
 
 int parse_title(char *s) {
 	gfx_printf("title will be %s\n", s);
+	
+	return 0;
+}
+
+int parse_boot() {
+	return 0;
+}
+int parse_reboot() {
+	return 0;
+}
+int parse_poweroff() {
+	return 0;
+}
+
+int parse_savedefault(char *s) {
+	return 0;
+}
+
+int parse_kernel(char *s) {
+	return 0;
+}
+
+int parse_root(char *s) {
+	return 0;
+}
+
+int parse_splashimage(char *s) {
+	return 0;
+}
+
+int parse_color(char *s) {
+	return 0;
+}
+
+int parse_find(char *s) {
+	return 0;
+}
+
+int process_line(char *line) {
+	// skip whitespace
+	while (isspace(*line))
+		line++;
+	if (!*line)
+		return 0;
+	
+	// is this a comment?
+	if (*line == '#')
+		return 0;
+		
+	// find next space or end of line
+	char *eot = line;
+	int eol_reached = 0;
+	while (1) {
+		if (!*eot) {
+			eol_reached = 1;
+			break;
+		}
+		if (!isspace(*eot))
+			eot++;
+		else {
+			*eot = 0x0;
+			eot++;
+			break;
+		}
+	}
+	
+	// skip some more leading whitespaces
+	if (!eol_reached) {
+		while (1) {
+			if (!*eot) {
+				break;
+			}
+			if (isspace(*eot))
+				eot++;
+			else
+				break;
+		}
+	}
+	
+	if (0 == strcmp(line, "timeout")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_timeout(eot);
+	} else if (0 == strcmp(line, "default")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_default(eot);
+	} else if (0 == strcmp(line, "title")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_title(eot);
+	} else if (0 == strcmp(line, "savedefault")) {
+		// extra argument is optional
+		return parse_savedefault(eot);
+	} else if (0 == strcmp(line, "splashimage")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_splashimage(eot);
+	} else if (0 == strcmp(line, "color")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_color(eot);
+	} else if (0 == strcmp(line, "kernel")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_kernel(eot);
+	} else if (0 == strcmp(line, "root")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_root(eot);
+	} else if (0 == strcmp(line, "find")) {
+		if (eol_reached) {
+			return ERR_MISSING_TOKEN;
+		}
+		
+		return parse_find(eot);
+	} else if (0 == strcmp(line, "reboot")) {
+		if (!eol_reached) {
+			return ERR_EXTRA_TOKEN;
+		}
+		
+		return parse_reboot();
+	} else if (0 == strcmp(line, "boot")) {
+		if (!eol_reached) {
+			return ERR_EXTRA_TOKEN;
+		}
+		
+		return parse_boot();
+	} else if ((0 == strcmp(line, "poweroff")) || (0 == strcmp(line, "halt"))) {
+		if (!eol_reached) {
+			return ERR_EXTRA_TOKEN;
+		}
+		
+		return parse_poweroff();
+	} else {
+		gfx_printf("unknown token: %s\n", line);
+	}
+	
+//	if (eol_reached)
+//		return 0;
 	
 	return 0;
 }
